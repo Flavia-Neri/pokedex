3eac207a23713b66b2b03729e34847bc2e7ac584
Flavia.Neri <flavia.sousaneri@gmail.com>
2021-04-07 19:18:24 -0300

    Pokemon card style

--- assets/script.js
+++ assets/script.js
@@ -22,9 +22,11 @@
 function render(char){
 
     const sectionPokemonContainer = document.createElement("section");
-
-    sectionPokemonContainer.style.background =
-      pokemonColors[char.types[0].type.name] || "#fff";
+    const sectionText = document.createElement("section");
+    const sectionImage = document.createElement("section");
+    const pId = document.createElement("p");
+    sectionPokemonContainer.style.backgroundColor =
+      pokemonColors[char.types[0].type.name] || "#c9c9c9";
 
     const strongName = document.createElement("strong");
     strongName.innerText = char.name;
@@ -34,32 +36,47 @@ function render(char){
     imagePokemon.src =
       char.sprites.other["official-artwork"].front_default;
 
-    const pTypeOne = document.createElement("p");
+    const pTypeOne = document.createElement("div");
     pTypeOne.innerText = char.types[0].type.name;
 
-    const pTypeTwo = document.createElement("p");
-    pTypeTwo.innerText = char.types[1].type.name;
+    pId.innerText = "#00"+char.id;
 
-    sectionPokemonContainer.appendChild(imagePokemon);
-    sectionPokemonContainer.appendChild(strongName);
-    sectionPokemonContainer.appendChild(pTypeOne);
-    sectionPokemonContainer.appendChild(pTypeTwo);
+    sectionPokemonContainer.style.boxShadow= "0px 5px 20px "+ pokemonColors[char.types[0].type.name] || "#c9c9c9";
+    sectionPokemonContainer.classList.add("sectionPokemonContainer");
+    sectionText.classList.add("sectionText");
+    sectionImage.classList.add("sectionImage");
+    pTypeOne.classList.add("typeOne");
+    imagePokemon.classList.add("imagePokemon");
+    pId.classList.add("pId");
 
+    sectionText.appendChild(strongName);
+    sectionText.appendChild(pTypeOne);
+    sectionImage.appendChild(pId);
+    sectionImage.appendChild(imagePokemon);
+    sectionPokemonContainer.appendChild(sectionText);
+    sectionPokemonContainer.appendChild(sectionImage);
+    
+    if(char.types.length > 1)
+    {
+        const pTypeTwo = document.createElement("div");
+        pTypeTwo.innerText = char.types[1].type.name;
+        pTypeTwo.classList.add("typeTwo");
+        sectionText.appendChild(pTypeTwo);
+    }
     main.appendChild(sectionPokemonContainer);
- 
 }
 function convertToObj(response){
     return response.json();
 }
 function listPokemons(jsonObj){
-    jsonObj.results.forEach(pokemon => {
-        fetch(pokemon.url)
-        .then(convertToObj)
-        .then(render)
-    })
+    const requests = jsonObj.results.map(pokemon => fetch(pokemon.url));
+    Promise.all(requests)
+    .then((responses)=> Promise.all(responses.map(r=> r.json())))
+    .then((pokemons)=>pokemons.forEach(pokemon=> render(pokemon)));
 }
-const generations = [151, 300, 400, 500, 600, 700, 800, 898];
+const generations = [151, 251, 386, 493, 649, 721, 809];
 let   counterGeneration = 0;
+
 function startLoad(){
     fetch("https://pokeapi.co/api/v2/pokemon?limit= "+ generations[counterGeneration++] + "&offset=" + (generations[counterGeneration - 2] || 0))
     .then(convertToObj)
